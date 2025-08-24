7c44708d8c09d09ab4de349ab668ad2d092099d7
dinhthanh143 <neonlimea@gmail.com>
2025-08-24 22:45:17 +0700

    Update Ex8.tsx

--- Session15/src/components/Ex8.tsx
+++ Session15/src/components/Ex8.tsx
@@ -16,7 +16,7 @@ export default class Ex8 extends Component<object, stateType> {
       clearInterval(this.timerId);
     }
     this.timerId = setInterval(() => {
-      this.setState((prev) => ({ count: prev.count + 1 }));
+      this.setState((prev) => ({ count: this.state.count >=10? 0 : prev.count + 1 }));
     }, 1000);
   }
 
@@ -25,14 +25,14 @@ export default class Ex8 extends Component<object, stateType> {
       clearInterval(this.timerId);
     }
   }
-  componentDidUpdate(prevProps: object, prevState: Readonly<stateType>): void {
-    if (prevState.count === 10) {
-      if (this.timerId) {
-        clearInterval(this.timerId);
-      }
-      this.setState(() => ({ count: 0 }));
-    }
-  }
+  // componentDidUpdate(prevProps: object, prevState: Readonly<stateType>): void {
+  //   if (prevState.count === 10) {
+  //     if (this.timerId) {
+  //       clearInterval(this.timerId);
+  //     }
+  //     this.setState(() => ({ count: 0 }));
+  //   }
+  // }
 
   render() {
     return <div>Count : {this.state.count}</div>;
